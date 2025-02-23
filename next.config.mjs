1bf15ac7f1a3257f318f30ebc21e4bdae4ea8708
Yashgupta9330 <yaahg342@gmail.com>
2025-02-23 13:37:51 +0530

    fixed config errorr

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = { typescript: {
-    ignoreBuildErrors: true,  // Ignores TypeScript errors only
-  },
-  output: 'export',  // This is important for Cloudflare Pages
-  images: {
-    unoptimized: true, // Required for static export
-  },
-  eslint: {
-    ignoreDuringBuilds: false,  // Still checks for ESLint errors
-  },};
-
-export default nextConfig;
+const nextConfig = {
+    typescript: {
+      ignoreBuildErrors: true,  // Ignores TypeScript errors only
+    },
+    images: {
+      unoptimized: true, // Required for static export
+    },
+    eslint: {
+      ignoreDuringBuilds: false,  // Still checks for ESLint errors
+    },
+    experimental: {
+      runtime: 'edge',
+    },
+  };
+  
+  export default nextConfig;
+  
